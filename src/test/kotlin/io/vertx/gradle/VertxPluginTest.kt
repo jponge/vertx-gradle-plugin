03971a43f9f13df3e4715a9e1e155e4363a164b0
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-08-24 15:47:18 +0200

    Test that a "fat" Jar file is being created

--- src/test/kotlin/io/vertx/gradle/VertxPluginTest.kt
+++ src/test/kotlin/io/vertx/gradle/VertxPluginTest.kt
@@ -3,6 +3,8 @@ package io.vertx.gradle
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Test
 import java.io.File
+import org.assertj.core.api.Assertions.*
+import java.util.jar.JarFile
 
 /**
  * @author [Julien Ponge](https://julien.ponge.org/)
@@ -11,10 +13,29 @@ class VertxPluginTest {
 
   @Test
   fun `smoke test`() {
-    GradleRunner.create()
+    val runner = GradleRunner.create()
       .withProjectDir(File("src/test/gradle/simple-project"))
       .withPluginClasspath()
       .withArguments("tasks")
       .build()
+
+    assertThat(runner.output).contains("shadowJar", "BUILD SUCCESSFUL")
+  }
+
+  @Test
+  fun `check that it builds a fat jar`() {
+    GradleRunner.create()
+      .withProjectDir(File("src/test/gradle/simple-project"))
+      .withPluginClasspath()
+      .withArguments("build")
+      .build()
+
+    val fatJarFile = File("src/test/gradle/simple-project/build/libs/simple-project-fat.jar")
+    assertThat(fatJarFile).exists().isFile()
+    JarFile(fatJarFile).use {
+      assertThat(it.getJarEntry("sample/App.class")).isNotNull()
+      assertThat(it.getJarEntry("io/vertx/core/Vertx.class")).isNotNull()
+      assertThat(it.getJarEntry("io/netty/channel/EventLoopGroup.class")).isNotNull()
+    }
   }
 }
