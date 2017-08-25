d89e61163e8ff892976378ecdffa2357f12e1d5e
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-08-25 23:42:34 +0200

    Redeploying vertxRun tasks
    (First iteration, more to fix)

--- src/main/kotlin/io/vertx/gradle/VertxPlugin.kt
+++ src/main/kotlin/io/vertx/gradle/VertxPlugin.kt
@@ -2,13 +2,14 @@ package io.vertx.gradle
 
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.JavaVersion
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+import org.gradle.api.*
 import org.gradle.api.plugins.ApplicationPlugin
 import org.gradle.api.plugins.ApplicationPluginConvention
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
+import java.io.File
 
 /**
  * @author [Julien Ponge](https://julien.ponge.org/)
@@ -22,6 +23,7 @@ class VertxPlugin : Plugin<Project> {
       defineJavaSourceCompatibility(project)
       defineMainClassName(project)
       configureShadowPlugin(project)
+      createVertxRunTask(project)
     }
   }
 
@@ -62,6 +64,46 @@ class VertxPlugin : Plugin<Project> {
       }
     }
   }
+
+  private fun createVertxRunTask(project: Project) {
+    val vertxExtension = project.vertxExtension()
+    val javaConvention = project.convention.getPlugin(JavaPluginConvention::class.java)
+    val mainSourceSet = javaConvention.sourceSets.getByName("main")
+
+    project.tasks.create("vertxRun", JavaExec::class.java).apply {
+      group = "Application"
+      description = "Runs this project as a Vert.x application"
+
+      workingDir(File(vertxExtension.workDirectory))
+      jvmArgs(vertxExtension.jvmArgs)
+      classpath(mainSourceSet.runtimeClasspath)
+      main = vertxExtension.launcher
+
+      if (vertxExtension.launcher == "io.vertx.core.Launcher") {
+        if (vertxExtension.mainVerticle.isBlank()) {
+          throw GradleException("Extension property vertx.mainVerticle must be specified when using io.vertx.core.Launcher as a launcher")
+        }
+        args("run", vertxExtension.mainVerticle)
+      }
+
+      if (vertxExtension.redeploy) {
+        args("--launcher-class", "io.vertx.core.Launcher") // FIXME
+        args("--redeploy")
+        vertxExtension.watch.forEach { args(it) }
+        args("--redeploy-grace-period", vertxExtension.redeployGracePeriod)
+        args("--redeploy-scan-period", vertxExtension.redeployScanPeriod)
+        args("--redeploy-termination-period", vertxExtension.redeployTerminationPeriod)
+        if (vertxExtension.onRedeploy.isNotBlank()) {
+          args("--on-redeploy", vertxExtension.onRedeploy)
+        }
+      }
+
+      vertxExtension.args.forEach { args(it) }
+      if (vertxExtension.config.isNotBlank()) {
+        args("--conf", vertxExtension.config)
+      }
+    }
+  }
 }
 
 open class VertxExtension {
@@ -69,13 +111,14 @@ open class VertxExtension {
   var launcher: String = "io.vertx.core.Launcher"
   var mainVerticle: String = ""
 
-  var runArgs = listOf("src/**/*")
+  var args = listOf<String>()
   var config: String = ""
   var workDirectory: String = ""
-  var jvmArgs: String = ""
+  var jvmArgs = listOf<String>()
 
   var redeploy: Boolean = true
-  var onRedeploy: String = ""
+  var watch = listOf("src/**/*")
+  var onRedeploy: String = "gradle classes" // FIXME
   var redeployScanPeriod: Long = 1000
   var redeployGracePeriod: Long = 1000
   var redeployTerminationPeriod: Long = 1000
@@ -84,6 +127,8 @@ open class VertxExtension {
 /*
 
  = Notes
+
+ Does not play well with daemons
 
  On redeploy, --launcher-class=(...) combined with `run io.vertx.core.Launcher` does auto-reload of customer
  launcher classes.
