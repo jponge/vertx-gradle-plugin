0b539756c9597d8c5c641dd6c46865a48a0a9477
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-09-04 11:06:59 +0200

    Move the extension class to a separate file

--- src/main/kotlin/io/vertx/gradle/VertxPlugin.kt
+++ src/main/kotlin/io/vertx/gradle/VertxPlugin.kt
@@ -4,7 +4,6 @@ import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import netflix.nebula.dependency.recommender.DependencyRecommendationsPlugin
 import netflix.nebula.dependency.recommender.provider.RecommendationProviderContainer
-import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.GradleException
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
@@ -18,6 +17,8 @@ import org.slf4j.LoggerFactory
 import java.io.File
 
 /**
+ * A Gradle plugin for Eclipse Vert.x projects.
+ *
  * @author [Julien Ponge](https://julien.ponge.org/)
  */
 class VertxPlugin : Plugin<Project> {
@@ -141,32 +142,3 @@ class VertxPlugin : Plugin<Project> {
   }
 }
 
-open class VertxExtension(private val project: Project) {
-
-  var vertxVersion = "3.4.2"
-
-  var launcher: String = "io.vertx.core.Launcher"
-  var mainVerticle: String = ""
-
-  var args = listOf<String>()
-  var config: String = ""
-  var workDirectory: String = project.projectDir.absolutePath
-  var jvmArgs = listOf<String>()
-
-  var redeploy: Boolean = true
-  var watch = listOf("${project.projectDir.absolutePath}/src/**/*")
-  var onRedeploy: String = findGradleScript()
-  var redeployScanPeriod: Long = 1000
-  var redeployGracePeriod: Long = 1000
-  var redeployTerminationPeriod: Long = 1000
-
-  private fun findGradleScript(): String {
-    val gradlewScript = if (Os.isFamily(Os.FAMILY_WINDOWS)) "gradlew.bat" else "gradlew"
-    val gradlewScriptFile = File(project.projectDir, gradlewScript)
-    return if (gradlewScriptFile.exists()) "${gradlewScriptFile.absolutePath} classes" else "gradle classes"
-  }
-
-  override fun toString(): String {
-    return "VertxExtension(project=$project, vertxVersion='$vertxVersion', launcher='$launcher', mainVerticle='$mainVerticle', args=$args, config='$config', workDirectory='$workDirectory', jvmArgs=$jvmArgs, redeploy=$redeploy, watch=$watch, onRedeploy='$onRedeploy', redeployScanPeriod=$redeployScanPeriod, redeployGracePeriod=$redeployGracePeriod, redeployTerminationPeriod=$redeployTerminationPeriod)"
-  }
-}
