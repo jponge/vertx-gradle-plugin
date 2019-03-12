7c793eb4131daf75f08b9eeadef02b347b3e5f1c
Julien Ponge <julien.ponge@gmail.com>
2019-03-12 15:54:49 +0100

    Copyright years update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /*
- * Copyright 2017-2018 Red Hat, Inc.
+ * Copyright 2017-2019 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
